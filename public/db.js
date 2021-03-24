663b9ed3dc243e7ebcc8e322674195150d3a0d73
Young Ji Kim <youjmik@gmail.com>
2021-03-24 16:32:58 -0400

    got files connected.

--- public/db.js
+++ public/db.js
@@ -1,39 +1,54 @@
-const indexedDB =
-window.indexedDB ||
-window.mozIndexDB||
-window.webkitIndexedDB ||
-window.msIndexedDB ||
-window.shimIndexedDB;
 
+let db;
+const request = indexedDB.open('budget', 1);
 
-let db
-const request = indexedDB.open('budget',1);
-
-request.onupgradeneeded = ({target}) => {
+request.onupgradeneeded = ({ target }) => {
     let db = target.result;
-    db.createObjectStore('pending',{autoIncrement: true})
+    db.createObjectStore('pending', { autoIncrement: true })
 };
 
-request.onsuccess = ({target}) => {
-    db= target.result;
+request.onsuccess = ({ target }) => {
+    db = target.result;
 
     if (navigator.onLine) {
         checkDatabase()
     }
 }
 
-request.onerror = function(e) {
-    console.log('Woops' + e.target.errorCode)
+request.onerror = function (event) {
+    console.log('Woops' + event.target.errorCode)
 }
 
-function saveRecord (record)  {
-const transaction = db.transaction(["pending"],"readWrite");
-const store = transaction.objectStore("pending");
-store.add(record)
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readWrite");
+    const store = transaction.objectStore("pending");
+    store.add(record)
 };
 
-function checkDatabase(){
-    const transaction = db.transaction(["pending"],"readWrite");
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readWrite");
     const store = transaction.objectStore("pending");
-    const getAll=store.getAll()
+    const getAll = store.getAll()
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch ("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: { Accept: "application/json,text/plain, */*",
+                "Content-Type": "application/json",
+
+                },
+            })
+            .then((response) => response.json())
+            .then(()=>{
+                const transaction = db.transaction(["pending"], "readWrite");
+                const store = transaction.objectStore("pending")
+                store.clear()
+            })
+        }
+    }
 }
+
+window.addEventListener("online",checkDatabase)
+
--- public/index.js
+++ public/index.js
@@ -1,11 +1,11 @@
-if('serviceWorker' in navigator) {
-  window.addEventListener('load',()=>{
-    navigator.serviceWorker.register('./service-worker.js')
-    .then((reg)=>{
-      console.log('Service Worker is registered',reg)
-    })
-  })
-}
+// if('serviceWorker' in navigator) {
+//   window.addEventListener('load',()=>{
+//     navigator.serviceWorker.register('./service-worker.js')
+//     .then((reg)=>{
+//       console.log('Service Worker is registered',reg)
+//     })
+//   })
+// }
 
 let transactions = [];
 let myChart;
