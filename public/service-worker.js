e01a26dd7a19b60e21331d1ec89dd6c4b45d39fe
Young Ji Kim <youjmik@gmail.com>
2021-03-25 17:48:35 -0400

    still finding trying to locate what that 1 Cache error means... all my coding is correct

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,14 @@
-// const FILES_TO_CACHE = [
-//     "/",
-//     "/db.js",
-//     "/index.html",
-//     "/index.js",
-//     "/style.css",
-//     "/icons/icon-192x192.png",
-//     "/icons/icon-512x512.png",
+const FILES_TO_CACHE = [
+    "/",
+    "/db.js",
+    "/index.html",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/style.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
 
-//   ];
-  
+  ];
   const CACHE_NAME = "static-cache-v2";
   const DATA_CACHE_NAME = "data-cache-v1";
   
@@ -17,18 +17,11 @@
     evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
         console.log("Your files were pre-cached successfully!");
-        return cache.addAll([
-          "/",
-            "/db.js",
-           "/index.html",
-            "/index.js",
-            "/style.css",
-            "/icons/icon-192x192.png",
-            "/icons/icon-512x512.png",
-          ]);
+        return cache.addAll(FILES_TO_CACHE);
+        
       })
+      
     );
-  
     self.skipWaiting();
   });
   
